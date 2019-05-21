8317450ecca81e5bcca0fa4a7937ca95935f34cc
292414551asd <272733689@qq.com>
2019-05-21 17:21:22 +0800

    first commit

---
+++ src/main/java/com/microtip/mqtt/Publish.java
@@ -0,0 +1,17 @@
+package com.microtip.mqtt;
+
+import com.microtip.util.PublishSubscribe;
+
+/**
+ * 发布者
+ */
+public class Publish {
+
+    public static void main(String[] args) {
+        PublishSubscribe.publish();
+
+    }
+
+
+
+}
---
+++ src/main/java/com/microtip/mqtt/Test1.java
@@ -0,0 +1,16 @@
+//package com.microtip.mqtt;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.annotation.PostConstruct;
+//@Configuration
+//public class Test1 {
+//    @Autowired
+//    @PostConstruct
+//    public void test(){
+//        System.out.println("test1==============================");
+//
+//    }
+//
+//}
